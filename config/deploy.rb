68b64668ac9488fa64eddfebdae7adf39a2f6947
Cam <cam@ferroni.net>
2011-05-10 07:16:22 -0700

    Updated Capistrano to use GIT

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,38 +1,32 @@
-set :application, "gobbledybook.com"
-set :user, "cameronf"
-set :local_repository,  "svn+gobbledybookssh://gobbledybook.com/home/repository/gobbledybook/trunk"
-set :repository,  "file:///home/repository/gobbledybook/trunk"
-set :port, 10001
-
+set :application, "gobbledybook"
+ 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
-set :deploy_to, "/home/public_html/gobbledybook"
-
+set :deploy_to, "/var/www/sites/gobbledybook.com"
+ 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
-# set :scm, :subversion
-set :use_sudo, false
-
-set :location, "gobbledybook.com"
-
-role :app, location
-role :web, location
-role :db,  location, :primary => true
-
-desc "Restart Thin"
-task :restart_thin do
-  sudo "/etc/init.d/thin restart"
+set :scm, :git
+set :repository, "/srv/gitosis/repositories/gobbledybook.git"
+set :branch, "master"
+set :deploy_via, :remote_cache
+ 
+set :user, 'deploy'
+set :ssh_options, { :forward_agent => true }
+ 
+role :app, "67.207.128.103"
+role :web, "67.207.128.103"
+role :db,  "67.207.128.103", :primary => true
+ 
+namespace :deploy do
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+ 
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
+  end
 end
-
-desc "copy over database.yml"
-task :create_database_yml do
-  run "cp #{deploy_to}/current/config/database.yml.example #{deploy_to}/current/config/database.yml"
-end
-
-after "deploy", "deploy:cleanup"
-after "deploy:migrations", "deploy:cleanup"
-after "deploy:cleanup", "create_database_yml"
-after "create_database_yml", "restart_thin"
-
